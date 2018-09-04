a2eab228d0e554e2817de9a24450f847fea23a07
Jonas Kvinge <jonas@jkvinge.net>
2018-09-04 17:39:10 +0200

    Update taglib to support DSF

--- src/taglib.mk
+++ src/taglib.mk
@@ -1,12 +1,12 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := taglib
-$(PKG)_WEBSITE  := https://developer.kde.org/~wheeler/taglib.html
+$(PKG)_WEBSITE  := http://taglib.org/
 $(PKG)_DESCR    := TagLib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := d8d56d3
-$(PKG)_CHECKSUM := eef49c9c2273f1b00299ef4fdcde2f1095cfa8ee4699365c6a6499b13cc3a072
-$(PKG)_GH_CONF  := taglib/taglib/branches/master
+$(PKG)_VERSION  := e3ea819
+$(PKG)_CHECKSUM := 6167055fff1c9e12ca44a231371ea4150f0a1d9426a68a645ea7cc5f9ab18c24
+$(PKG)_GH_CONF  := jonaski/taglib/branches/master
 $(PKG)_DEPS     := cc zlib
 
 define $(PKG)_BUILD
