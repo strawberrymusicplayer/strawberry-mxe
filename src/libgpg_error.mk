c8c9f74ee580a7714d54bbe7f0af059806927eb1
Jonas Kvinge <jonas@jkvinge.net>
2019-01-25 23:33:44 +0100

    Update libgpg_error

--- src/libgpg_error.mk
+++ src/libgpg_error.mk
@@ -4,8 +4,8 @@ PKG             := libgpg_error
 $(PKG)_WEBSITE  := https://www.gnupg.org/related_software/libgpg-error/
 $(PKG)_DESCR    := libgpg-error
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.32
-$(PKG)_CHECKSUM := c345c5e73cc2332f8d50db84a2280abfb1d8f6d4f1858b9daa30404db44540ca
+$(PKG)_VERSION  := 1.34
+$(PKG)_CHECKSUM := 0680799dee71b86b2f435efb825391eb040ce2704b057f6bd3dcc47fbc398c81
 $(PKG)_SUBDIR   := libgpg-error-$($(PKG)_VERSION)
 $(PKG)_FILE     := libgpg-error-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://gnupg.org/ftp/gcrypt/libgpg-error/$($(PKG)_FILE)
@@ -19,15 +19,14 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)/src/syscfg' && ln -s lock-obj-pub.mingw32.h lock-obj-pub.mingw32.static.h
-    cd '$(1)/src/syscfg' && ln -s lock-obj-pub.mingw32.h lock-obj-pub.mingw32.shared.h
+    cd '$(1)/src/syscfg' && ln -s lock-obj-pub.mingw32.h lock-obj-pub.mingw32.$(call merge,.,$(call rest,$(call split,.,$(TARGET)))).h
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --disable-nls \
         --disable-languages
-    $(SED) -i 's/-lgpg-error/-lgpg-error -lintl -liconv -lws2_32/;' '$(1)/src/gpg-error-config'
     $(SED) -i 's/host_os = mingw32.*/host_os = mingw32/' '$(1)/src/Makefile'
     $(MAKE) -C '$(1)/src' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    $(SED) -i 's/-lgpg-error/-lgpg-error -lintl -liconv -lws2_32/;' '$(1)/src/gpg-error-config'
     $(MAKE) -C '$(1)/src' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     ln -sf '$(PREFIX)/$(TARGET)/bin/gpg-error-config' '$(PREFIX)/bin/$(TARGET)-gpg-error-config'
 endef
