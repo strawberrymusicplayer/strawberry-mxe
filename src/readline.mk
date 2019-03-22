e7bde835e9bb080141b3f7e632e777d58ac808dc
Jonas Kvinge <jonas@jkvinge.net>
2019-03-22 23:32:38 +0100

    Update readline

--- src/readline.mk
+++ src/readline.mk
@@ -4,24 +4,19 @@ PKG             := readline
 $(PKG)_WEBSITE  := https://tiswww.case.edu/php/chet/readline/rltop.html
 $(PKG)_DESCR    := Readline
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.3
-$(PKG)_CHECKSUM := 56ba6071b9462f980c5a72ab0023893b65ba6debb4eeb475d7a563dc65cafd43
+$(PKG)_VERSION  := 8.0
+$(PKG)_CHECKSUM := e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461
 $(PKG)_SUBDIR   := readline-$($(PKG)_VERSION)
 $(PKG)_FILE     := readline-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/readline/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc termcap
 
 define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
+    $(WGET) -q -O- 'https://tiswww.case.edu/php/chet/readline/rltop.html' | \
+    grep 'readline-' | \
+    $(SED) -n 's,.*readline-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
 endef
-
-#define $(PKG)_UPDATE
-#    $(WGET) -q -O- 'https://tiswww.case.edu/php/chet/readline/rltop.html' | \
-#    grep 'readline-' | \
-#    $(SED) -n 's,.*readline-\([0-9][^>]*\)\.tar.*,\1,p' | \
-#    head -1
-#endef
 
 define $(PKG)_BUILD
     cd '$(1)' && bash_cv_wcwidth_broken=no \
