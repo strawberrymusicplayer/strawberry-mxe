5ac9cf082e9c8a3510c315eb7512e0f3eb046c8f
Jonas Kvinge <jonas@jkvinge.net>
2019-03-31 03:39:03 +0200

    Update taglib

--- src/taglib.mk
+++ src/taglib.mk
@@ -1,7 +1,7 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := taglib
-$(PKG)_WEBSITE  := http://taglib.org/
+$(PKG)_WEBSITE  := https://taglib.org/
 $(PKG)_DESCR    := TagLib
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 6455671
