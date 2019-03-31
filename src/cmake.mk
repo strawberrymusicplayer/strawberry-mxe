38ac0c048d78118c4201f66853ffaf718ff455cf
Jonas Kvinge <jonas@jkvinge.net>
2019-03-31 14:50:46 +0200

    Update cmake

--- src/cmake.mk
+++ src/cmake.mk
@@ -3,8 +3,8 @@
 PKG             := cmake
 $(PKG)_WEBSITE  := https://www.cmake.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.13.4
-$(PKG)_CHECKSUM := fdd928fee35f472920071d1c7f1a6a2b72c9b25e04f7a37b409349aef3f20e9b
+$(PKG)_VERSION  := 3.14.1
+$(PKG)_CHECKSUM := 7321be640406338fc12590609c42b0fae7ea12980855c1be363d25dcd76bb25f
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
@@ -12,16 +12,11 @@ $(PKG)_TARGETS  := $(BUILD)
 $(PKG)_DEPS     :=
 
 define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
+    $(WGET) -q -O- 'https://www.cmake.org/cmake/resources/software.html' | \
+    $(SED) -n 's,.*cmake-\([0-9.]*\)\.tar.*,\1,p' | \
+    $(SORT) -V | \
+    tail -1
 endef
-
-#define $(PKG)_UPDATE
-#    $(WGET) -q -O- 'https://www.cmake.org/cmake/resources/software.html' | \
-#    $(SED) -n 's,.*cmake-\([0-9.]*\)\.tar.*,\1,p' | \
-#    $(SORT) -V | \
-#    tail -1
-#endef
 
 define $(PKG)_BUILD_$(BUILD)
     mkdir '$(1).build'
