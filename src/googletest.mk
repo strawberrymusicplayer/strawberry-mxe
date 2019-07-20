af300249cd034062d43ec0f03e17b21dcc4126f9
Jonas Kvinge <jonas@jkvinge.net>
2019-07-20 02:12:18 +0200

    Enable googletest update

--- src/googletest.mk
+++ src/googletest.mk
@@ -4,16 +4,9 @@ PKG             := googletest
 $(PKG)_WEBSITE  := https://github.com/google/googletest
 $(PKG)_DESCR    := Google Test
 $(PKG)_IGNORE   :=
-#$(PKG)_VERSION  := 1.8.0
-#$(PKG)_CHECKSUM := 58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8
-$(PKG)_VERSION  := 1.7.0
-$(PKG)_CHECKSUM := f73a6546fdf9fce9ff93a5015e0333a8af3062a152a9ad6bcb772c96687016cc
+$(PKG)_VERSION  := 1.8.1
+$(PKG)_CHECKSUM := 9bf1fe5182a604b4135edc1a425ae356c9ad15e9b23f9f12a02e80184c3a249c
 $(PKG)_GH_CONF  := google/googletest/tags, release-
 $(PKG)_DEPS     :=
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 $(PKG)_TYPE     := source-only
-
-define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
-endef
