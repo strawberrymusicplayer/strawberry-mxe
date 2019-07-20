0fc7205ade3a2ca053fa9c32da81ed57dac37c8c
Jonas Kvinge <jonas@jkvinge.net>
2019-07-20 02:20:57 +0200

    Add qtimageformats dependency to qtdeclarative

--- src/qtdeclarative.mk
+++ src/qtdeclarative.mk
@@ -9,8 +9,10 @@ $(PKG)_CHECKSUM := b9e8780aef0af4a60e64dcc405bdf5c03a04b28e3b94d5c2e69d0006db566
 $(PKG)_SUBDIR    = $(subst qtbase,qtdeclarative,$(qtbase_SUBDIR))
 $(PKG)_FILE      = $(subst qtbase,qtdeclarative,$(qtbase_FILE))
 $(PKG)_URL       = $(subst qtbase,qtdeclarative,$(qtbase_URL))
-$(PKG)_DEPS     := cc qtbase qtsvg qtxmlpatterns
+$(PKG)_DEPS     := cc qtbase qtimageformats qtsvg qtxmlpatterns
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+
+$(PKG)_DEPS_$(BUILD) := qtbase qtsvg qtxmlpatterns
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
