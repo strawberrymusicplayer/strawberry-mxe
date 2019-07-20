8dd773833f911c46f3cfb588e6fc133ddc94ee41
Jonas Kvinge <jonas@jkvinge.net>
2019-07-20 02:06:32 +0200

    Add jasper

---
+++ src/jasper.mk
@@ -0,0 +1,22 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := jasper
+$(PKG)_WEBSITE  := https://www.ece.uvic.ca/~mdadams/jasper/
+$(PKG)_DESCR    := JasPer
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.0.16
+$(PKG)_CHECKSUM := f1d8b90f231184d99968f361884e2054a1714fdbbd9944ba1ae4ebdcc9bbfdb1
+$(PKG)_GH_CONF  := mdadams/jasper/tags, version-
+$(PKG)_DEPS     := cc jpeg
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DJAS_ENABLE_SHARED=$(CMAKE_SHARED_BOOL) \
+        -DJAS_ENABLE_LIBJPEG=ON \
+        -DJAS_ENABLE_OPENGL=OFF \
+        -DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=OFF \
+        -DJAS_ENABLE_DOC=OFF \
+        -DJAS_ENABLE_PROGRAMS=OFF
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+endef
