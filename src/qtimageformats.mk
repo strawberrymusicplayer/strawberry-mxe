e399e7af061820bab1ffc17a8f08532d223c5537
Jonas Kvinge <jonas@jkvinge.net>
2019-07-20 02:08:39 +0200

    Add qtimageformats

---
+++ src/qtimageformats.mk
@@ -0,0 +1,22 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qtimageformats
+$(PKG)_WEBSITE  := https://www.qt.io/
+$(PKG)_DESCR    := Qt
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION   = $(qtbase_VERSION)
+$(PKG)_CHECKSUM := 91cb638e5f856acc17ab5a2e1a052dc91bb1af2bdbd7d99ba6a8a48f4a15f499
+$(PKG)_SUBDIR    = $(subst qtbase,qtimageformats,$(qtbase_SUBDIR))
+$(PKG)_FILE      = $(subst qtbase,qtimageformats,$(qtbase_FILE))
+$(PKG)_URL       = $(subst qtbase,qtimageformats,$(qtbase_URL))
+$(PKG)_DEPS     := cc jasper libmng libwebp qtbase tiff
+
+define $(PKG)_UPDATE
+    echo $(qtbase_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
