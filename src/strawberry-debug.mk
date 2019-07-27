f94405eeb4a589a0e533856b7e2f7c7a547b091a
Jonas Kvinge <jonas@jkvinge.net>
2019-07-27 13:20:26 +0200

    Update strawberry

--- src/strawberry-debug.mk
+++ src/strawberry-debug.mk
@@ -2,8 +2,8 @@
 
 PKG             := strawberry-debug
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0a71347
-$(PKG)_CHECKSUM := 0b5c5b58518051467391bb6cec4eeccef2a260b89c1d9db89a660e389f062343
+$(PKG)_VERSION  := 8e9c980
+$(PKG)_CHECKSUM := 2718a3211f81aece1cb06b6c5b5318f7ed4c3b9405ff2ce28cab0e4818a58559
 $(PKG)_GH_CONF  := jonaski/strawberry/branches/master
 $(PKG)_WEBSITE  := https://www.strawbs.org/
 $(PKG)_OWNER    := https://github.com/jonaski
