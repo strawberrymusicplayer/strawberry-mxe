42aef414271f4f6c821416f2bb99fd0054d39d77
Jonas Kvinge <jonas@jkvinge.net>
2019-09-02 17:29:51 +0200

    Update glib

--- src/glib.mk
+++ src/glib.mk
@@ -4,83 +4,31 @@ PKG             := glib
 $(PKG)_WEBSITE  := https://gtk.org/
 $(PKG)_DESCR    := GLib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.58.3
-$(PKG)_CHECKSUM := 8f43c31767e88a25da72b52a40f3301fefc49a665b56dc10ee7cc9565cbe7481
+$(PKG)_VERSION  := 2.61.2
+$(PKG)_CHECKSUM := 99c6bcfb8600562d519222a429e2dc0af469b2006b8080a9a1f438456071c0b5
 $(PKG)_SUBDIR   := glib-$($(PKG)_VERSION)
 $(PKG)_FILE     := glib-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/glib/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc dbus gettext libffi libiconv pcre zlib $(BUILD)~$(PKG)
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 
-$(PKG)_DEPS_$(BUILD) := autotools gettext libffi libiconv zlib
+$(PKG)_DEPS_$(BUILD) := meson ninja gettext libffi libiconv zlib
 
 define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
-endef
-
-#define $(PKG)_UPDATE
-#    $(call MXE_GET_GH_TAGS,GNOME/glib) | \
-#    $(SED) -n 's,^\([0-9]*\.[0-9]*[02468]\..*\),\1,p' | \
-#    $(SORT) -Vr | \
-#    head -1
-#endef
-
-define $(PKG)_BUILD_DARWIN
-    # native build for glib-tools
-    cd '$(SOURCE_DIR)' && NOCONFIGURE=true ./autogen.sh
-    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
-        $(MXE_CONFIGURE_OPTS) \
-        --enable-regex \
-        --disable-threads \
-        --disable-selinux \
-        --disable-inotify \
-        --disable-fam \
-        --disable-xattr \
-        --disable-dtrace \
-        --disable-libmount \
-        --with-pcre=internal \
-        PKG_CONFIG='$(PREFIX)/$(TARGET)/bin/pkgconf' \
-        CPPFLAGS='-I$(PREFIX)/$(TARGET).gnu/include' \
-        LDFLAGS='-L$(PREFIX)/$(TARGET).gnu/lib'
-    $(MAKE) -C '$(BUILD_DIR)/glib'    -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gthread' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gmodule' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gobject' -j '$(JOBS)' lib_LTLIBRARIES= install-exec
-    $(MAKE) -C '$(BUILD_DIR)/gio/xdgmime'     -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gio/kqueue'      -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gio'     -j '$(JOBS)' glib-compile-schemas
-    $(MAKE) -C '$(BUILD_DIR)/gio'     -j '$(JOBS)' glib-compile-resources
-    $(INSTALL) -m755 '$(BUILD_DIR)/gio/glib-compile-schemas' '$(PREFIX)/$(TARGET)/bin/'
-    $(INSTALL) -m755 '$(BUILD_DIR)/gio/glib-compile-resources' '$(PREFIX)/$(TARGET)/bin/'
+    $(call MXE_GET_GH_TAGS,GNOME/glib) | \
+    $(SED) -n 's,^\([0-9]*\.[0-9]*[02468]\..*\),\1,p' | \
+    $(SORT) -Vr | \
+    head -1
 endef
 
 define $(PKG)_BUILD_NATIVE
-    # native build for glib-tools
-    cd '$(SOURCE_DIR)' && NOCONFIGURE=true ./autogen.sh
-    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
-        $(MXE_CONFIGURE_OPTS) \
-        --disable-selinux \
-        --disable-fam \
-        --disable-xattr \
-        --disable-dtrace \
-        --disable-libmount \
-        --with-libiconv=gnu \
-        --with-pcre=internal \
-        PKG_CONFIG='$(PREFIX)/$(TARGET)/bin/pkgconf' \
-        CPPFLAGS='-I$(PREFIX)/$(TARGET)/include' \
-        LDFLAGS='-L$(PREFIX)/$(TARGET)/lib'
-    $(SED) -i 's,#define G_ATOMIC.*,,' '$(BUILD_DIR)/config.h'
-    $(MAKE) -C '$(BUILD_DIR)/glib'    -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gthread' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gmodule' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gobject' -j '$(JOBS)' lib_LTLIBRARIES= install-exec
-    $(MAKE) -C '$(BUILD_DIR)/gio/xdgmime'     -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gio'     -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/gio'     -j '$(JOBS)' glib-compile-schemas
-    $(MAKE) -C '$(BUILD_DIR)/gio'     -j '$(JOBS)' glib-compile-resources
-    $(INSTALL) -m755 '$(BUILD_DIR)/gio/glib-compile-schemas' '$(PREFIX)/$(TARGET)/bin/'
-    $(INSTALL) -m755 '$(BUILD_DIR)/gio/glib-compile-resources' '$(PREFIX)/$(TARGET)/bin/'
+    cd '$(SOURCE_DIR)' && $(PREFIX)/x86_64-pc-linux-gnu/bin/meson \
+                                --prefix='$(PREFIX)/$(TARGET)' \
+                                --buildtype=release \
+                                --pkg-config-path='$(PREFIX)/$(TARGET)/bin/pkgconf' \
+                                '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && ninja
+    cd '$(BUILD_DIR)' && ninja install
 endef
 
 define $(PKG)_BUILD_$(BUILD)
@@ -96,24 +44,14 @@ define $(PKG)_BUILD
     ln -sf '$(PREFIX)/$(BUILD)/bin/glib-compile-schemas'   '$(PREFIX)/$(TARGET)/bin/'
     ln -sf '$(PREFIX)/$(BUILD)/bin/glib-compile-resources' '$(PREFIX)/$(TARGET)/bin/'
 
-    # cross build
-    cd '$(SOURCE_DIR)' && NOCONFIGURE=true ./autogen.sh
-    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
-        $(MXE_CONFIGURE_OPTS) \
-        --with-threads=win32 \
-        --with-pcre=system \
-        --with-libiconv=gnu \
-        --disable-inotify \
-        CXX='$(TARGET)-g++' \
-        PKG_CONFIG='$(PREFIX)/bin/$(TARGET)-pkg-config' \
-        GLIB_GENMARSHAL='$(PREFIX)/$(TARGET)/bin/glib-genmarshal' \
-        GLIB_COMPILE_SCHEMAS='$(PREFIX)/$(TARGET)/bin/glib-compile-schemas' \
-        GLIB_COMPILE_RESOURCES='$(PREFIX)/$(TARGET)/bin/glib-compile-resources'
-    $(MAKE) -C '$(BUILD_DIR)/glib'    -j '$(JOBS)' install sbin_PROGRAMS= noinst_PROGRAMS=
-    $(MAKE) -C '$(BUILD_DIR)/gmodule' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
-    $(MAKE) -C '$(BUILD_DIR)/gthread' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
-    $(MAKE) -C '$(BUILD_DIR)/gobject' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
-    $(MAKE) -C '$(BUILD_DIR)/gio'     -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= MISC_STUFF=
-    $(MAKE) -C '$(BUILD_DIR)'         -j '$(JOBS)' install-pkgconfigDATA
-    $(MAKE) -C '$(BUILD_DIR)/m4macros' install
+    cd '$(SOURCE_DIR)' && $(PREFIX)/x86_64-pc-linux-gnu/bin/meson \
+                                --cross-file '$(PREFIX)/$(TARGET)/share/meson/mxe-crossfile.meson' \
+                                --prefix='$(PREFIX)/$(TARGET)' \
+                                --buildtype=release \
+                                --pkg-config-path='$(PREFIX)/$(TARGET)/bin/pkgconf' \
+                                '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && ninja
+    cd '$(BUILD_DIR)' && ninja install
+
+
 endef
