7aea657a42578a8abfe6a2b568b6dcfdf03cf709
Jonas Kvinge <jonas@jkvinge.net>
2019-09-02 17:29:58 +0200

    Update glib-networking

--- src/glib-networking.mk
+++ src/glib-networking.mk
@@ -1,30 +1,42 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
 PKG             := glib-networking
 $(PKG)_WEBSITE  := https://www.gnome.org
 $(PKG)_DESCR    := Network-related GIO modules for glib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.54.1
-$(PKG)_CHECKSUM := eaa787b653015a0de31c928e9a17eb57b4ce23c8cf6f277afaec0d685335012f
+$(PKG)_VERSION  := 2.61.1
+$(PKG)_CHECKSUM := a3acbe8953ba80e408bdc4a3e8c240fd9447181c7e800a175c3105604c38bad5
 $(PKG)_SUBDIR   := glib-networking-$($(PKG)_VERSION)
 $(PKG)_FILE     := glib-networking-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/$(PKG)/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc gnutls
 
 define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
+   $(WGET) -q -O- 'https://git.gnome.org/browse/glib-networking/refs/tags' | \
+    grep '<a href=' | \
+    $(SED) -n 's,.*<a[^>]*>\([0-9]*\.[0-9]*[02468]\.[^<]*\)<.*,\1,p' | \
+    sort -Vr | \
+    head -1
 endef
 
-#define $(PKG)_UPDATE
-#   $(WGET) -q -O- 'https://git.gnome.org/browse/glib-networking/refs/tags' | \
-#    grep '<a href=' | \
-#    $(SED) -n 's,.*<a[^>]*>\([0-9]*\.[0-9]*[02468]\.[^<]*\)<.*,\1,p' | \
-#    sort -Vr | \
-#    head -1
-#endef
+define $(PKG)_BUILD_NATIVE
+    cd '$(SOURCE_DIR)' && $(PREFIX)/x86_64-pc-linux-gnu/bin/meson \
+                                --prefix='$(PREFIX)/$(TARGET)' \
+                                --buildtype=release \
+                                --pkg-config-path='$(PREFIX)/$(TARGET)/bin/pkgconf' \
+                                '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && ninja
+    cd '$(BUILD_DIR)' && ninja install
+
+endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
-	$(subst docdir$(comma),,$(MXE_CONFIGURE_OPTS))
-    $(MAKE) -C '$(1)'
-    $(MAKE) -C '$(1)' install
+    cd '$(SOURCE_DIR)' && $(PREFIX)/x86_64-pc-linux-gnu/bin/meson \
+                                --cross-file='$(PREFIX)/$(TARGET)/share/meson/mxe-crossfile.meson' \
+                                --prefix='$(PREFIX)/$(TARGET)' \
+                                --buildtype=release \
+                                --pkg-config-path='$(PREFIX)/$(TARGET)/bin/pkgconf' \
+                                '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && ninja
+    cd '$(BUILD_DIR)' && ninja install
 endef
