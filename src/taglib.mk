31f0c0de673c30e19aef10b8c10d66899f5c4edf
Jonas Kvinge <jonas@jkvinge.net>
2019-09-13 00:08:31 +0200

    Update taglib

--- src/taglib.mk
+++ src/taglib.mk
@@ -4,15 +4,10 @@ PKG             := taglib
 $(PKG)_WEBSITE  := https://taglib.org/
 $(PKG)_DESCR    := TagLib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := fced0f4
-$(PKG)_CHECKSUM := 9a9d4d69f939862cdd007e61076d2929a82bef1e5dae1f68a067c31a73790a60
+$(PKG)_VERSION  := ebd3373
+$(PKG)_CHECKSUM := 26fcddd367e4de07becb131d2be93e487719f1df71269aa154ed274272ad20a7
 $(PKG)_GH_CONF  := taglib/taglib/branches/master
 $(PKG)_DEPS     := cc zlib
-
-define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
-endef
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)'
