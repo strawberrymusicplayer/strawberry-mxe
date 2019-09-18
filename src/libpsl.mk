dc798126f3a62f44d7bb8d29c78623c06250623b
Jonas Kvinge <jonas@jkvinge.net>
2019-09-18 22:10:06 +0200

    Fix update for libpsl

--- src/libpsl.mk
+++ src/libpsl.mk
@@ -5,15 +5,11 @@ $(PKG)_WEBSITE  := http://www.linuxfromscratch.org/blfs/view/svn/basicnet/libpsl
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 0.21.0
 $(PKG)_CHECKSUM := 41bd1c75a375b85c337b59783f5deb93dbb443fb0a52d257f403df7bd653ee12
+$(PKG)_GH_CONF  := rockdaboot/libpsl/tags, libpsl-
 $(PKG)_SUBDIR   := libpsl-$($(PKG)_VERSION)
 $(PKG)_FILE     := libpsl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/rockdaboot/libpsl/releases/download/$(PKG)-$($(PKG)_VERSION)/$(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := cc
-
-define $(PKG)_UPDATE
-    echo 'TODO: Updates for package $(PKG) need to be written.' >&2;
-    echo $($(PKG)_VERSION)
-endef
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS)
