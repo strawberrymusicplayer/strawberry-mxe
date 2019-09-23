71e08c5ed318d2fbf0769cb71d73510504ba2da9
Jonas Kvinge <jonas@jkvinge.net>
2019-09-23 22:52:47 +0200

    Update libsoup to 2.68.1

--- src/libsoup.mk
+++ src/libsoup.mk
@@ -4,24 +4,19 @@ PKG             := libsoup
 $(PKG)_WEBSITE  := https://github.com/GNOME/libsoup
 $(PKG)_DESCR    := HTTP client/server library for GNOME
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.67.3
-$(PKG)_CHECKSUM := 4c948ddd4317ab631543429d4bdb7c79ee53f7f35c8153b54e59bc6ee8b5ef78
+$(PKG)_VERSION  := 2.68.1
+$(PKG)_CHECKSUM := 7f0323e53b8e797baa972dfe25adf3cc6ceff06f0a26235b6c5c7b91403fbf8d
 $(PKG)_SUBDIR   := libsoup-$($(PKG)_VERSION)
 $(PKG)_FILE     := libsoup-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/libsoup/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc meson ninja glib libxml2 sqlite brotli libpsl
+$(PKG)_DEPS     := cc meson ninja glib glib-networking libxml2 sqlite brotli libpsl
 
 define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
+    $(WGET) -q -O- 'https://gitlab.gnome.org/GNOME/libsoup/-/tags' | \
+    $(SED) -n "s,.*libsoup-\([0-9]\+\.[0-9]*[02468]*\.[^']*\)\.tar.*,\1,p" | \
+    $(SORT) -Vr | \
+    head -1
 endef
-
-#define $(PKG)_UPDATE
-#    $(WGET) -q -O- 'https://gitlab.gnome.org/GNOME/libsoup/-/tags' | \
-#    $(SED) -n "s,.*libsoup-\([0-9]\+\.[0-9]*[02468]*\.[^']*\)\.tar.*,\1,p" | \
-#    $(SORT) -Vr | \
-#    head -1
-#endef
 
 define $(PKG)_BUILD
     cd '$(SOURCE_DIR)' && $(PREFIX)/x86_64-pc-linux-gnu/bin/meson \
@@ -30,8 +25,9 @@ define $(PKG)_BUILD
                                 --buildtype=release \
                                 --pkg-config-path='$(PREFIX)/$(TARGET)/bin/pkgconf' \
                                 -Dtests=false \
-                                -Dvapi=false \
-                                -Dgssapi=false \
+                                -Dvapi=disabled \
+                                -Dgssapi=disabled \
+                                -Dintrospection=disabled \
                                 '$(BUILD_DIR)'
     cd '$(BUILD_DIR)' && ninja
     cd '$(BUILD_DIR)' && ninja install
