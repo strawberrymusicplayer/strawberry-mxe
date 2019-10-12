73244b92e02194d9b4bf02f237d2d6602c98b7f3
Jonas Kvinge <jonas@jkvinge.net>
2019-10-12 20:01:47 +0200

    Add singleapplication

---
+++ src/singleapplication.mk
@@ -0,0 +1,20 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := singleapplication
+$(PKG)_WEBSITE  := https://github.com/itay-grudev/SingleApplication
+$(PKG)_DESCR    := SingleApplication
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 3.0.17
+$(PKG)_CHECKSUM := 48a0176230186839d11ddb8aefd1db34a78af448a45eb00352d328c314257c53
+$(PKG)_GH_CONF  := itay-grudev/SingleApplication/tags
+$(PKG)_DEPS     := cc
+
+define $(PKG)_UPDATE
+    echo $(singleapplication_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' -DQAPPLICATION_CLASS="QApplication"
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+endef
