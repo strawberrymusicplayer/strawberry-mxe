5b0902617f0b389c05f90c115277c8dcf0c7d1e4
Jonas Kvinge <jonas@jkvinge.net>
2019-10-12 14:23:44 +0200

    Update glib-networking

--- src/glib-networking.mk
+++ src/glib-networking.mk
@@ -4,15 +4,15 @@ PKG             := glib-networking
 $(PKG)_WEBSITE  := https://www.gnome.org
 $(PKG)_DESCR    := Network-related GIO modules for glib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.62.1
-$(PKG)_CHECKSUM := 3c55ae6771ad7a79fa606a834f4686ed555c2774ed6e9ece6f3c0f6a3dab7110
+$(PKG)_VERSION  := 2.63.1
+$(PKG)_CHECKSUM := 148c9549a6608084db5b2d3645cc5236dc1630c4ad37c8650ce25dd413b57d6f
 $(PKG)_SUBDIR   := glib-networking-$($(PKG)_VERSION)
 $(PKG)_FILE     := glib-networking-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/$(PKG)/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc meson ninja glib gnutls
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://gitlab.gnome.org/GNOME/glib-networking/-/tags' | \
+    $(WGET) -q -O- -t 2 --timeout=6 'https://gitlab.gnome.org/GNOME/glib-networking/-/tags' | \
     $(SED) -n "s,.*glib-networking-\([0-9]\+\.[0-9]*[0-9]*\.[^']*\)\.tar.*,\1,p" | \
     $(SORT) -Vr | \
     head -1
