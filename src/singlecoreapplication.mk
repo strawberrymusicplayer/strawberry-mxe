2458ed83658215e81cbf7728c23e10bb41425bab
Jonas Kvinge <jonas@jkvinge.net>
2019-10-13 11:53:01 +0200

    Fix singleapplication dependency

--- src/singlecoreapplication.mk
+++ src/singlecoreapplication.mk
@@ -7,7 +7,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 3.0.17
 $(PKG)_CHECKSUM := 48a0176230186839d11ddb8aefd1db34a78af448a45eb00352d328c314257c53
 $(PKG)_GH_CONF  := itay-grudev/SingleApplication/tags
-$(PKG)_DEPS     := cc cmake qtbase
+$(PKG)_DEPS     := cc qtbase
 
 define $(PKG)_UPDATE
     echo $(singlecoreapplication_VERSION)
