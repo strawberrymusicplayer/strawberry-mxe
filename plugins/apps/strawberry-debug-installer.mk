ce66386aa4c2d866213aa5b82cda845824cdf6fa
Jonas Kvinge <jonas@jkvinge.net>
2019-11-16 20:58:50 +0100

    Add nsis plugins

--- plugins/apps/strawberry-debug-installer.mk
+++ plugins/apps/strawberry-debug-installer.mk
@@ -2,9 +2,12 @@
 
 PKG             := strawberry-debug-installer
 $(PKG)_IGNORE   :=
-$(PKG)_DEPS     := strawberry-debug
+$(PKG)_DEPS     := strawberry-debug nsis-plugin-killproc nsis-plugin-shellexecasuser
 
 define $(PKG)_BUILD_SHARED
+  $(INSTALL) -d '$(PREFIX)/$(TARGET)/apps/strawberry-debug/bin/nsisplugins'
+  $(INSTALL) '$(PREFIX)/$(TARGET)/bin/KillProc.dll' '$(PREFIX)/$(TARGET)/apps/strawberry-debug/bin/nsisplugins'
+  $(INSTALL) '$(PREFIX)/$(TARGET)/bin/ShellExecAsUser.dll' '$(PREFIX)/$(TARGET)/apps/strawberry-debug/bin/nsisplugins'
   makensis '$(PREFIX)/$(TARGET)/apps/strawberry-debug/bin/strawberry.nsi'
 endef
 
--- plugins/apps/strawberry-installer.mk
+++ plugins/apps/strawberry-installer.mk
@@ -2,9 +2,12 @@
 
 PKG             := strawberry-installer
 $(PKG)_IGNORE   :=
-$(PKG)_DEPS     := strawberry
+$(PKG)_DEPS     := strawberry nsis-plugin-killproc nsis-plugin-shellexecasuser
 
 define $(PKG)_BUILD_SHARED
+  $(INSTALL) -d '$(PREFIX)/$(TARGET)/apps/strawberry/bin/nsisplugins'
+  $(INSTALL) '$(PREFIX)/$(TARGET)/bin/KillProc.dll' '$(PREFIX)/$(TARGET)/apps/strawberry/bin/nsisplugins'
+  $(INSTALL) '$(PREFIX)/$(TARGET)/bin/ShellExecAsUser.dll' '$(PREFIX)/$(TARGET)/apps/strawberry/bin/nsisplugins'
   makensis '$(PREFIX)/$(TARGET)/apps/strawberry/bin/strawberry.nsi'
 endef
 
