3d220093622c0c9ec88fcd4cead320daf325770d
Jonas Kvinge <jonas@jkvinge.net>
2019-12-22 15:48:28 +0100

    Update libplist

--- src/libplist.mk
+++ src/libplist.mk
@@ -3,21 +3,14 @@
 PKG             := libplist
 $(PKG)_WEBSITE  := https://github.com/libimobiledevice/libplist
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.0
-$(PKG)_CHECKSUM := 187eb8a7aca2d5abcdfd81c42ac12e67ebe49c434299df1881a942e64b7c7978
+$(PKG)_VERSION  := 2.1.0
+$(PKG)_CHECKSUM := 4b33f9af3f9208d54a3c3e1a8c149932513f451c98d1dd696fe42c06e30b7f03
 $(PKG)_GH_CONF  := libimobiledevice/libplist/tags
 $(PKG)_DEPS     := cc libxml2
 
-define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
-endef
-
 define $(PKG)_BUILD
-    cd '$(SOURCE_DIR)' && NOCONFIGURE=true $(SHELL) ./autogen.sh
-    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
-        $(MXE_CONFIGURE_OPTS) \
-        --without-cython
-    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+    cd '$(1)' && NOCONFIGURE=true $(SHELL) ./autogen.sh
+    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS) --without-cython
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
 endef
