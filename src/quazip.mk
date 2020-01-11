ef0a6529347ff5e62100cab06523a2a61916db36
Jonas Kvinge <jonas@jkvinge.net>
2020-01-11 18:22:09 +0100

    quazip: use qmake instead

--- src/quazip.mk
+++ src/quazip.mk
@@ -10,7 +10,22 @@ $(PKG)_GH_CONF  := stachenov/quazip/tags, v
 $(PKG)_DEPS     := cc qtbase
 
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)'
+    cd '$(BUILD_DIR)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake' '$(SOURCE_DIR)' \
+        'static:CONFIG += staticlib' \
+        PREFIX=$(PREFIX)/$(TARGET) \
+        -after \
+        'win32:LIBS_PRIVATE += -lz' \
+        'CONFIG -= dll' \
+        'CONFIG += create_prl no_install_prl create_pc' \
+        'QMAKE_PKGCONFIG_DESTDIR = pkgconfig' \
+        'static:QMAKE_PKGCONFIG_CFLAGS += -DQUAZIP_STATIC' \
+        'DESTDIR = ' \
+        'DLLDESTDIR = ' \
+        'win32:dlltarget.path = $(PREFIX)/$(TARGET)/bin' \
+        'target.path = $(PREFIX)/$(TARGET)/lib'  \
+        '!static:win32:target.CONFIG = no_dll' \
+        'win32:INSTALLS += dlltarget' \
+        'INSTALLS += target headers'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
