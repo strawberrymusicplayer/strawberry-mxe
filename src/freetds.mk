4e2b8d2431d2d67fd5d22cf78a29b2590fe567a2
Jonas Kvinge <jonas@jkvinge.net>
2020-01-19 23:18:30 +0100

    Add freetds

---
+++ src/freetds.mk
@@ -0,0 +1,36 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := freetds
+$(PKG)_WEBSITE  := https://www.freetds.org/
+$(PKG)_DESCR    := FreeTDS
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.1.24
+$(PKG)_CHECKSUM := b327367553c2cd2467361efbb2ded71edb8492747d2605ffde652738c386cb40
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := https://www.freetds.org/files/stable/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://fossies.org/linux/privat/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc openssl libiconv
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://www.freetds.org/files/stable/' | \
+    $(SED) -n 's,.*freetds-\([0-9.]*\)\.tar.*,\1,p' | \
+    $(SORT) -V | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --disable-rpath \
+        --disable-dependency-tracking \
+        --enable-libiconv \
+        --enable-msdblib \
+        --enable-sspi \
+        --disable-threadsafe \
+        --with-tdsver=7.2 \
+        --with-openssl \
+        PKG_CONFIG='$(TARGET)-pkg-config' \
+        CFLAGS=-D_WIN32_WINNT=0x0600
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install man_MANS=
+endef
