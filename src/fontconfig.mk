f5f3063454b5a2d4a0ab1e17cff4be1cc47502a5
Jonas Kvinge <jonas@jkvinge.net>
2020-02-06 18:24:11 +0100

    Downgrade fontconfig to latest stable release

--- src/fontconfig.mk
+++ src/fontconfig.mk
@@ -3,16 +3,17 @@
 PKG             := fontconfig
 $(PKG)_WEBSITE  := https://fontconfig.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.13.92
-$(PKG)_CHECKSUM := 506e61283878c1726550bc94f2af26168f1e9f2106eac77eaaf0b2cdfad66e4e
+$(PKG)_VERSION  := 2.13.1
+$(PKG)_CHECKSUM := f655dd2a986d7aa97e052261b36aa67b0a64989496361eca8d604e6414006741
 $(PKG)_SUBDIR   := fontconfig-$($(PKG)_VERSION)
-$(PKG)_FILE     := fontconfig-$($(PKG)_VERSION).tar.xz
+$(PKG)_FILE     := fontconfig-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://fontconfig.org/release/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc expat freetype-bootstrap gettext
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://fontconfig.org/release/' | \
     $(SED) -n 's,.*fontconfig-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    grep -v '\([0-9]\+\.\)\{2\}9[0-9]' | \
     tail -1
 endef
 
