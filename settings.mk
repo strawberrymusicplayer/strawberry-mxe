6d619f6a8687fb2cb199ec208f86fb15125ed735
Jonas Kvinge <jonas@jkvinge.net>
2020-02-29 16:29:53 +0100

    Disable ccache

--- settings.mk
+++ settings.mk
@@ -28,3 +28,5 @@ MXE_TARGETS := i686-w64-mingw32.shared x86_64-w64-mingw32.shared
 #LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
 #.DEFAULT_GOAL  := local-pkg-list
 #local-pkg-list: $(LOCAL_PKG_LIST)
+
+MXE_USE_CCACHE :=
