3d02f8778c8f3aa0ad31cdf93a14ef5e9303be49
Jonas Kvinge <jonas@jkvinge.net>
2020-05-01 14:24:11 +0200

    Update pe-parse

--- src/pe-parse.mk
+++ src/pe-parse.mk
@@ -4,20 +4,15 @@ PKG             := pe-parse
 $(PKG)_WEBSITE  := https://github.com/trailofbits/pe-parse
 $(PKG)_DESCR    := Principled, lightweight C/C++ PE parser
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.2
-$(PKG)_CHECKSUM := 4a80620bf73fce802be40e2131c3118ba31f7724fbeddee5e6a48cd0d8388a6e
-$(PKG)_GH_CONF  := trailofbits/pe-parse/releases
+$(PKG)_VERSION  := 1.2.0
+$(PKG)_CHECKSUM := c2fe84a8944b36f2952b575d6517cbcf9590d6c5759702d78b64dbbf3482c515
+$(PKG)_GH_CONF  := trailofbits/pe-parse/releases, v
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 $(PKG)_DEPS     := cc
 
 # unpack in pe-util for native and cross build
 $(PKG)_DEPS_$(BUILD)  :=
 $(PKG)_BUILD_$(BUILD) :=
-
-define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
-endef
 
 define $(PKG)_BUILD
     # build and install the cross-library
@@ -30,5 +25,5 @@ define $(PKG)_BUILD
     mkdir '$(BUILD_DIR).cmake'
     cd '$(BUILD_DIR).cmake' && $(TARGET)-cmake '$(SOURCE_DIR)/examples/peaddrconv'
     $(MAKE) -C '$(BUILD_DIR).cmake' -j '$(JOBS)'
-    $(INSTALL) -m755 '$(BUILD_DIR).cmake/peaddrconv.exe' '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe'
+    #$(INSTALL) -m755 '$(BUILD_DIR).cmake/peaddrconv.exe' '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe'
 endef
