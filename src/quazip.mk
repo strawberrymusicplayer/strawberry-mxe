bccd26acb3cc6273234840a1fae9b9165b01cd19
Jonas Kvinge <jonas@jkvinge.net>
2020-05-01 14:24:20 +0200

    Update quazip

--- src/quazip.mk
+++ src/quazip.mk
@@ -4,8 +4,8 @@ PKG             := quazip
 $(PKG)_WEBSITE  := https://github.com/stachenov/quazip
 $(PKG)_DESCR    := quazip
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8e61e04
-$(PKG)_CHECKSUM := d738661c48cc9214fd4ef4c8049c34ad0279f8af32e141b54ff65519bb777681
+$(PKG)_VERSION  := 13348ef
+$(PKG)_CHECKSUM := e47fac135f63633c0257c2ee820c91ac617b1f1f2ef6522899d610ad48ad054f
 $(PKG)_GH_CONF  := stachenov/quazip/branches/master
 $(PKG)_DEPS     := cc qtbase zlib
 
@@ -13,13 +13,13 @@ define $(PKG)_BUILD_STATIC
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
-    rm -f $(PREFIX)/$(TARGET)/lib/libquazip*.dll*
+    rm -f $(PREFIX)/$(TARGET)/lib/libQt5Quazip*.dll
 endef
 
 define $(PKG)_BUILD_SHARED
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
-    mv $(PREFIX)/$(TARGET)/lib/libquazip*.dll $(PREFIX)/$(TARGET)/bin/
-    rm -f $(PREFIX)/$(TARGET)/lib/libquazip*.a
+    #mv $(PREFIX)/$(TARGET)/lib/libQt5Quazip*.dll $(PREFIX)/$(TARGET)/bin/
+    rm -f $(PREFIX)/$(TARGET)/lib/libQt5Quazip*.a
 endef
