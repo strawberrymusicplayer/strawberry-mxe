b180b10138c4aaa37d5ffe7d7d8fd94bb3713a0c
Jonas Kvinge <jonas@jkvinge.net>
2020-05-29 20:16:56 +0200

    Add meson as a dependency for fribidi

--- src/fribidi.mk
+++ src/fribidi.mk
@@ -7,7 +7,9 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.0.9
 $(PKG)_CHECKSUM := ef6f940d04213a0fb91a0177b2b57df2031bf3a7e2cd0ee2c6877a160fc206df
 $(PKG)_GH_CONF  := fribidi/fribidi/releases, v
-$(PKG)_DEPS     := cc meson-conf ninja
+$(PKG)_DEPS     := cc meson-conf ninja $(BUILD)~meson
+
+$(PKG)_DEPS_$(BUILD) := meson ninja
 
 define $(PKG)_BUILD
     cd '$(SOURCE_DIR)' && $(TARGET)-meson '$(BUILD_DIR)' -Ddocs=false -Ddeprecated=false
