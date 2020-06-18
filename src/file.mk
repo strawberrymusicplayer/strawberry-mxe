49df313dc7ca2cbb4ac861feef0174e5d7495c59
Jonas Kvinge <jonas@jkvinge.net>
2020-06-18 21:28:21 +0200

    Update file

--- src/file.mk
+++ src/file.mk
@@ -3,8 +3,8 @@
 PKG             := file
 $(PKG)_WEBSITE  := https://www.darwinsys.com/file/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.38
-$(PKG)_CHECKSUM := 593c2ffc2ab349c5aea0f55fedfe4d681737b6b62376a9b3ad1e77b2cc19fa34
+$(PKG)_VERSION  := 5.39
+$(PKG)_CHECKSUM := f05d286a76d9556243d0cb05814929c2ecf3a5ba07963f8f70bfaaa70517fad1
 $(PKG)_SUBDIR   := file-$($(PKG)_VERSION)
 $(PKG)_FILE     := file-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://astron.com/pub/file/$($(PKG)_FILE)
