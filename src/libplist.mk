2a6d328b9425d28aad70baf6cad20b49c873f16e
Jonas Kvinge <jonas@jkvinge.net>
2020-06-18 21:29:49 +0200

    Update libplist

--- src/libplist.mk
+++ src/libplist.mk
@@ -3,8 +3,8 @@
 PKG             := libplist
 $(PKG)_WEBSITE  := https://github.com/libimobiledevice/libplist
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.1.0
-$(PKG)_CHECKSUM := 4b33f9af3f9208d54a3c3e1a8c149932513f451c98d1dd696fe42c06e30b7f03
+$(PKG)_VERSION  := 2.2.0
+$(PKG)_CHECKSUM := 7e654bdd5d8b96f03240227ed09057377f06ebad08e1c37d0cfa2abe6ba0cee2
 $(PKG)_GH_CONF  := libimobiledevice/libplist/tags
 $(PKG)_DEPS     := cc libxml2
 
