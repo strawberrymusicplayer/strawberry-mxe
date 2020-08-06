b995fcc1643c341bc49967b65581bc047abc51a3
Jonas Kvinge <jonas@jkvinge.net>
2020-08-06 17:53:22 +0200

    Update dlfcn-win32

--- src/dlfcn-win32.mk
+++ src/dlfcn-win32.mk
@@ -4,8 +4,8 @@ PKG             := dlfcn-win32
 $(PKG)_WEBSITE  := https://github.com/dlfcn-win32/dlfcn-win32
 $(PKG)_DESCR    := POSIX dlfcn wrapper for Windows
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0a494c0
-$(PKG)_CHECKSUM := d7f88819262074fbe3081a144773952f494987d6394e8ecf18df173cbec0fae5
+$(PKG)_VERSION  := e5d9fab
+$(PKG)_CHECKSUM := 71b97f960798f14ef252b0429be82cb98bdcc3b1b600662d08108a04944403ad
 $(PKG)_GH_CONF  := dlfcn-win32/dlfcn-win32/branches/master
 $(PKG)_DEPS     := cc
 
