c493cef8f7ca5dc131ad042b9108a4b8d047bab7
Jonas Kvinge <jonas@jkvinge.net>
2020-08-27 15:39:16 +0200

    Update brotli

--- src/brotli.mk
+++ src/brotli.mk
@@ -4,8 +4,8 @@ PKG             := brotli
 $(PKG)_WEBSITE  := https://opensource.google.com/projects/brotli
 $(PKG)_DESCR    := Brotli compression format
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.7
-$(PKG)_CHECKSUM := 4c61bfb0faca87219ea587326c467b95acb25555b53d1a421ffa3c8a9296ee2c
+$(PKG)_VERSION  := 1.0.8
+$(PKG)_CHECKSUM := 6dcc73364b1a1ee7e49024d25e00b8ca4968396a81af99c8c6eb8757b52f74ea
 $(PKG)_GH_CONF  := google/brotli/tags, v
 $(PKG)_DEPS     := cc
 
