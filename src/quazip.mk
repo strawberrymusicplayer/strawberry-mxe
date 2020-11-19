46a47ddfeb1b4c97e003a6b513712b4dd8e0f513
Jonas Kvinge <jonas@jkvinge.net>
2020-11-19 18:05:32 +0100

    Update quazip

--- src/quazip.mk
+++ src/quazip.mk
@@ -4,8 +4,8 @@ PKG             := quazip
 $(PKG)_WEBSITE  := https://github.com/stachenov/quazip
 $(PKG)_DESCR    := quazip
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := dcfeed2
-$(PKG)_CHECKSUM := f1efb5374ab3ce76c65769d49230d4ec3a843da4b68a83c86b55aef7e101d7c9
+$(PKG)_VERSION  := f7d8abb
+$(PKG)_CHECKSUM := 70c7e63f7267080a0a22b3256f387e17df43f9ed293d81530797436036ad0599
 $(PKG)_GH_CONF  := stachenov/quazip/branches/master
 $(PKG)_DEPS     := cc qtbase zlib
 
