0304045d68d784cdecb2012afab530d40305c82a
Jonas Kvinge <jonas@jkvinge.net>
2021-01-08 17:35:25 +0100

    Update taglib

--- src/taglib.mk
+++ src/taglib.mk
@@ -4,8 +4,8 @@ PKG             := taglib
 $(PKG)_WEBSITE  := https://taglib.org/
 $(PKG)_DESCR    := TagLib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 30d8395
-$(PKG)_CHECKSUM := 3b0d01f0ece14ccd846acc18914d51b5b559636dce47de3dd6a105fb0c3f7f81
+$(PKG)_VERSION  := 89fb62c
+$(PKG)_CHECKSUM := a90849ecf1f5ba07dbde5c3917b3a0384c474d8708764960bb1d9e3dbb81c332
 $(PKG)_GH_CONF  := taglib/taglib/branches/master
 $(PKG)_DEPS     := cc zlib
 
