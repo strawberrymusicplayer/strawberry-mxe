213322fe255aea47b6a21d5b8b3223cecccf3db2
Jonas Kvinge <jonas@jkvinge.net>
2021-01-15 16:31:49 +0100

    Add Qt 6 Core 5 Compat

---
+++ src/qt6qt5compat.mk
@@ -0,0 +1,29 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qt6qt5compat
+$(PKG)_WEBSITE  := https://www.qt.io/
+$(PKG)_DESCR    := Qt 6 Qt 5 Compat
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 6.0.0
+$(PKG)_CHECKSUM := 13b9d78aa698609d07376e3e9e9d1a82a96084236aba0642f8ac695c181ab25f
+$(PKG)_FILE     := qt5compat-everywhere-src-$($(PKG)_VERSION).tar.xz
+$(PKG)_SUBDIR   := qt5compat-everywhere-src-$($(PKG)_VERSION)
+$(PKG)_URL      := https://download.qt.io/official_releases/qt/6.0/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
+$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+$(PKG)_DEPS     := cc qt6base
+$(PKG)_DEPS_$(BUILD) :=
+$(PKG)_OO_DEPS_$(BUILD) += qt6-conf ninja
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- https://download.qt.io/official_releases/qt/6.0/ | \
+    $(SED) -n 's,.*href="\(6\.0\.[^/]*\)/".*,\1,p' | \
+    grep -iv -- '-rc' | \
+    sort |
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    $(QT6_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    cmake --build '$(BUILD_DIR)' -j '$(JOBS)'
+    cmake --install '$(BUILD_DIR)'
+endef
