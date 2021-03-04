505591f254ae5083a3a6246f31f3678bab098726
Jonas Kvinge <jonas@jkvinge.net>
2021-03-04 22:00:07 +0100

    Update libsoup

--- src/libsoup.mk
+++ src/libsoup.mk
@@ -4,8 +4,8 @@ PKG             := libsoup
 $(PKG)_WEBSITE  := https://github.com/GNOME/libsoup
 $(PKG)_DESCR    := HTTP client/server library for GNOME
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.72.0
-$(PKG)_CHECKSUM := 170c3f8446b0f65f8e4b93603349172b1085fb8917c181d10962f02bb85f5387
+$(PKG)_VERSION  := 2.99.1
+$(PKG)_CHECKSUM := 9703c09e1b41d413bc17b5a3b8baac7cd8be1aa89ebd628de802d9a572dc8d44
 $(PKG)_SUBDIR   := libsoup-$($(PKG)_VERSION)
 $(PKG)_FILE     := libsoup-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/libsoup/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
@@ -15,7 +15,6 @@ define $(PKG)_UPDATE
     $(call MXE_GET_GH_TAGS,GNOME/libsoup) | \
     grep -v '\([0-9]\+\.\)\{2\}9[0-9]' | \
     $(SORT) -Vr | \
-    grep -v '2.99.1' | \
     head -1
 endef
 
