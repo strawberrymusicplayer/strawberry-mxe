ea6492eb2f57e0a6659222a91daa6e80e038efb0
Jonas Kvinge <jonas@jkvinge.net>
2021-03-13 00:01:13 +0100

    Add qt5activeqt

---
+++ src/qt5activeqt.mk
@@ -0,0 +1,27 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qt5activeqt
+$(PKG)_WEBSITE  := https://www.qt.io/
+$(PKG)_DESCR    := Qt 5 Active Qt
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 5.15.2
+$(PKG)_CHECKSUM := 868161fee0876d17079cd5bed58d1667bf19ffd0018cbe515129f11510ad2a5c
+$(PKG)_SUBDIR   := qtactiveqt-everywhere-src-$($(PKG)_VERSION)
+$(PKG)_FILE     := qtactiveqt-everywhere-src-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://download.qt.io/official_releases/qt/5.15/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc qt5base
+$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- https://download.qt.io/official_releases/qt/5.15/ | \
+    $(SED) -n 's,.*href="\(5\.15\.[^/]*\)/".*,\1,p' | \
+    grep -iv -- '-rc' | \
+    sort |
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
