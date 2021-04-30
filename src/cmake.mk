119be616b3cb7a94e9d79c2b02550691c3aeb7e9
Jonas Kvinge <jonas@jkvinge.net>
2021-05-01 01:13:50 +0200

    Update cmake

--- src/cmake.mk
+++ src/cmake.mk
@@ -3,8 +3,8 @@
 PKG             := cmake
 $(PKG)_WEBSITE  := https://www.cmake.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.20.1
-$(PKG)_CHECKSUM := 3f1808b9b00281df06c91dd7a021d7f52f724101000da7985a401678dfe035b0
+$(PKG)_VERSION  := 3.20.2
+$(PKG)_CHECKSUM := aecf6ecb975179eb3bb6a4a50cae192d41e92b9372b02300f9e8f1d5f559544e
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
