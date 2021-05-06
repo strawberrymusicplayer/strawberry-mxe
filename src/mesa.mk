6ba32a17948a6d59c26a521a29007df4174f7435
Jonas Kvinge <jonas@jkvinge.net>
2021-05-06 21:42:53 +0200

    Update mesa

--- src/mesa.mk
+++ src/mesa.mk
@@ -3,31 +3,23 @@
 PKG             := mesa
 $(PKG)_WEBSITE  := https://mesa3d.org
 $(PKG)_DESCR    := The Mesa 3D Graphics Library
-$(PKG)_VERSION  := 21.0.3
-$(PKG)_CHECKSUM := 565c6f4bd2d5747b919454fc1d439963024fc78ca56fd05158c3b2cde2f6912b
+$(PKG)_VERSION  := 21.1.0
+$(PKG)_CHECKSUM := 0128f10e22970d3aed3d1034003731f94623015cd9797c07151417649c1b1ff8
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.freedesktop.org/pub/mesa/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc scons-local
+$(PKG)_DEPS     := cc meson-conf $(BUILD)~meson zlib
 
 define $(PKG)_UPDATE
     $(call GET_LATEST_VERSION, https://archive.mesa3d.org, mesa-)
 endef
 
 define $(PKG)_BUILD
-    $(SCONS_PREP)
-    cd '$(SOURCE_DIR)' && \
-    MINGW_PREFIX='$(TARGET)-' $(SCONS_LOCAL) \
-        platform=windows \
-        toolchain=crossmingw \
-        machine=$(if $(findstring x86_64,$(TARGET)),x86_64,x86) \
-        verbose=1 \
-        build=release \
-        libgl-gdi
-
+    cd '$(SOURCE_DIR)' && $(TARGET)-meson '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && ninja
+    cd '$(BUILD_DIR)' && ninja install
     for i in EGL GLES GLES2 GLES3 KHR; do \
         $(INSTALL) -d "$(PREFIX)/$(TARGET)/include/$$i"; \
         $(INSTALL) -m 644 "$(1)/include/$$i/"* "$(PREFIX)/$(TARGET)/include/$$i/"; \
     done
-    $(INSTALL) -m 755 '$(1)/build/windows-$(if $(findstring x86_64,$(TARGET)),x86_64,x86)/gallium/targets/libgl-gdi/opengl32.dll' '$(PREFIX)/$(TARGET)/bin/'
 endef
