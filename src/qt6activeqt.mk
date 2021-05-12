b8c1f66406423631851ae9ebcc346e7f3cadbf65
jonas@jkvinge.net <jonas@jkvinge.net>
2021-05-12 23:27:12 +0200

    Add qt6activeqt

---
+++ src/qt6activeqt.mk
@@ -0,0 +1,27 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qt6activeqt
+$(PKG)_WEBSITE  := https://www.qt.io/
+$(PKG)_DESCR    := Qt 6 Active Qt
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 6.1.0
+$(PKG)_CHECKSUM := d79f964054a98b7edcfcb3f3590a58d599bcb2f9569ec10f5f5d9c0070701b4a
+$(PKG)_SUBDIR   := qtactiveqt-everywhere-src-$($(PKG)_VERSION)
+$(PKG)_FILE     := qtactiveqt-everywhere-src-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://download.qt.io/official_releases/qt/6.1/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc qt5base
+$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- https://download.qt.io/official_releases/qt/6.1/ | \
+    $(SED) -n 's,.*href="\(6\.1\.[^/]*\)/".*,\1,p' | \
+    grep -iv -- '-rc' | \
+    sort |
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    $(QT6_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    cmake --build '$(BUILD_DIR)' -j '$(JOBS)'
+    cmake --install '$(BUILD_DIR)'
+endef
