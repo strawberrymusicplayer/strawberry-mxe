7ef9b6a8a92b3228d7b6225368d2a5bdb38a6052
Jonas Kvinge <jonas@jkvinge.net>
2021-05-26 16:46:30 +0200

    Update t6translations

--- src/qt6translations.mk
+++ src/qt6translations.mk
@@ -4,16 +4,16 @@ PKG             := qt6translations
 $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt 6 Translations
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.0.4
-$(PKG)_CHECKSUM := ad8a9b17af98fb1633a5d698f84e80767d84328cf481fe36458c6fe430842caa
+$(PKG)_VERSION  := 6.1.0
+$(PKG)_CHECKSUM := 2affef9a0da9b61e8fc64ebbd515136b59c7392b789cd059ad134b26755dd073
 $(PKG)_FILE     := qttranslations-everywhere-src-$($(PKG)_VERSION).tar.xz
 $(PKG)_SUBDIR   := qttranslations-everywhere-src-$($(PKG)_VERSION)
-$(PKG)_URL      := https://download.qt.io/official_releases/qt/6.0/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
+$(PKG)_URL      := https://download.qt.io/official_releases/qt/6.1/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc qt6base qt6tools
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- https://download.qt.io/official_releases/qt/6.0/ | \
-    $(SED) -n 's,.*href="\(6\.0\.[^/]*\)/".*,\1,p' | \
+    $(WGET) -q -O- https://download.qt.io/official_releases/qt/6.1/ | \
+    $(SED) -n 's,.*href="\(6\.1\.[^/]*\)/".*,\1,p' | \
     grep -iv -- '-rc' | \
     sort |
     tail -1
