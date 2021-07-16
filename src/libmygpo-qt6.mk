11e64a49e8a6e16e84d9ddf7411d43da6690d986
Jonas Kvinge <jonas@jkvinge.net>
2021-07-16 06:42:10 +0200

    Add libmygpo

---
+++ src/libmygpo-qt6.mk
@@ -0,0 +1,16 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := libmygpo-qt6
+$(PKG)_WEBSITE  := https://github.com/gpodder/libmygpo-qt
+$(PKG)_DESCR    := libmygpo Qt 6
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := ba5db4d
+$(PKG)_CHECKSUM := 69ed06e061bea6fdcebe461382b9abeaf72b5dddcdab95ec87520c261fed0400
+$(PKG)_GH_CONF  := gpodder/libmygpo-qt/branches/master
+$(PKG)_DEPS     := cc qt6-qtbase
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' -DBUILD_SHARED_LIBS=$(CMAKE_SHARED_BOOL) -DCMAKE_PREFIX_PATH=$(PREFIX)/$(TARGET)/qt6/lib/cmake -DMYGPO_BUILD_TESTS=OFF
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+endef
