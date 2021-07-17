93eb4ca66e577f5bac3de8ac4fe4d3e8f65e45b2
Jonas Kvinge <jonas@jkvinge.net>
2021-07-17 19:50:50 +0200

    Fix libmygpo-qt6 dependency

--- src/libmygpo-qt6.mk
+++ src/libmygpo-qt6.mk
@@ -7,7 +7,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := ba5db4d
 $(PKG)_CHECKSUM := 69ed06e061bea6fdcebe461382b9abeaf72b5dddcdab95ec87520c261fed0400
 $(PKG)_GH_CONF  := gpodder/libmygpo-qt/branches/master
-$(PKG)_DEPS     := cc qt6-qtbase
+$(PKG)_DEPS     := cc qt6-qtbase qt6-qttools
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' -DBUILD_SHARED_LIBS=$(CMAKE_SHARED_BOOL) -DCMAKE_PREFIX_PATH=$(PREFIX)/$(TARGET)/qt6/lib/cmake -DMYGPO_BUILD_TESTS=OFF
