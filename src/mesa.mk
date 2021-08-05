de9ebdaa013cf066ebd2b86ce674c24bd430158b
Jonas Kvinge <jonas@jkvinge.net>
2021-08-05 22:47:23 +0200

    Update mesa

--- src/mesa.mk
+++ src/mesa.mk
@@ -3,8 +3,8 @@
 PKG             := mesa
 $(PKG)_WEBSITE  := https://mesa3d.org
 $(PKG)_DESCR    := The Mesa 3D Graphics Library
-$(PKG)_VERSION  := 21.1.6
-$(PKG)_CHECKSUM := b1cb0122f911dfa9fe1f209a7061378c61a1448a280e711511ca72dad9999e37
+$(PKG)_VERSION  := 21.2.0
+$(PKG)_CHECKSUM := 0cb3c802f4b8e7699b1602c08c29d06a4d532ab5b8f7a64676c4ca6bb8f4d426
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.freedesktop.org/pub/mesa/$($(PKG)_FILE)
