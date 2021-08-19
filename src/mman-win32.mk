720a76f79ced758d836ff3bb373ce6d0ad62fdc8
Jonas Kvinge <jonas@jkvinge.net>
2021-08-19 22:55:40 +0200

    Update checksum for mman-win32

--- src/mman-win32.mk
+++ src/mman-win32.mk
@@ -5,7 +5,7 @@ $(PKG)_WEBSITE  := https://code.google.com/p/mman-win32/
 $(PKG)_DESCR    := MMA-Win32
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2d1c576
-$(PKG)_CHECKSUM := 3b112c8084a29102759e53971ebf1d4d9df48f01c452d0742e1b1569f660f14e
+$(PKG)_CHECKSUM := b9b7a4afdf19644b761bd66a7a0fba5cea4bdf2cba180bd9323a6029f134ccbe
 $(PKG)_GH_CONF  := witwall/mman-win32/branches/master
 $(PKG)_DEPS     := cc
 
