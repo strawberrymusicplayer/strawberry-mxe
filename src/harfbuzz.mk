9cb8d34fa6a6a26425c72f34460ad57db76cc27a
Jonas Kvinge <jonas@jkvinge.net>
2021-09-07 19:42:05 +0200

    Update harfbuzz

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.9.0
-$(PKG)_CHECKSUM := bf5d5bad69ee44ff1dd08800c58cb433e9b3bf4dad5d7c6f1dec5d1cf0249d04
+$(PKG)_VERSION  := 2.9.1
+$(PKG)_CHECKSUM := f997abd0e3b90647408bfb01af2383b5fc9557af20137e7c2deaf2fa13705dc8
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
 $(PKG)_DEPS     := cc cairo freetype-bootstrap glib icu4c
 
