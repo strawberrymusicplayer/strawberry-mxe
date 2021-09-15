19e5f317c9f6c955a375c306209c524189ae4914
Jonas Kvinge <jonas@jkvinge.net>
2021-09-15 20:24:40 +0200

    Update fftw

--- src/fftw.mk
+++ src/fftw.mk
@@ -3,8 +3,8 @@
 PKG             := fftw
 $(PKG)_WEBSITE  := http://www.fftw.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.3.9
-$(PKG)_CHECKSUM := bf2c7ce40b04ae811af714deb512510cc2c17b9ab9d6ddcf49fe4487eea7af3d
+$(PKG)_VERSION  := 3.3.10
+$(PKG)_CHECKSUM := 56c932549852cddcfafdab3820b0200c7742675be92179e59e6215b340e26467
 $(PKG)_SUBDIR   := fftw-$($(PKG)_VERSION)
 $(PKG)_FILE     := fftw-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.fftw.org/$($(PKG)_FILE)
