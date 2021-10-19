a969cd30a6859a70ad1058f7a11cfa7aa05aded0
Jonas Kvinge <jonas@jkvinge.net>
2021-10-19 20:20:58 +0200

    Update nghttp2

--- src/nghttp2.mk
+++ src/nghttp2.mk
@@ -4,8 +4,8 @@ PKG             := nghttp2
 $(PKG)_WEBSITE  := https://nghttp2.org/
 $(PKG)_DESCR    := HTTP/2 C Library and tools
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.45.1
-$(PKG)_CHECKSUM := 6289eed7e83988428c6b24e46ada52a37fab82f20d4afa257b5627267c2a141b
+$(PKG)_VERSION  := 1.46.0
+$(PKG)_CHECKSUM := 0875a638d319cd28b06dcc410e6dc2add1a52f7cab6f62b26025c448f8ae8f43
 $(PKG)_GH_CONF  := nghttp2/nghttp2/tags, v
 $(PKG)_DEPS     := cc
 
