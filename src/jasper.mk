a3398dd38ff1c415e8193c989d87d549f0037b92
Jonas Kvinge <jonas@jkvinge.net>
2022-02-14 21:27:37 +0100

    Update jasper

--- src/jasper.mk
+++ src/jasper.mk
@@ -4,8 +4,8 @@ PKG             := jasper
 $(PKG)_WEBSITE  := https://ece.engr.uvic.ca/~frodo/jasper/
 $(PKG)_DESCR    := JasPer Image Coding Toolkit
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.1
-$(PKG)_CHECKSUM := dccd8be8bbe4de036a3bc69840475ae1e80d7c28a94cb7eb029961d50539e37a
+$(PKG)_VERSION  := 3.0.2
+$(PKG)_CHECKSUM := 3280c7b48ad53f956ce22ce719ac23ca7812cdeff0667e3914a5bc22592ad43f
 $(PKG)_GH_CONF  := jasper-software/jasper/releases, version-
 $(PKG)_DEPS     := cc jpeg
 
@@ -17,7 +17,8 @@ define $(PKG)_BUILD
         -DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=OFF \
         -DJAS_ENABLE_DOC=OFF \
         -DJAS_ENABLE_PROGRAMS=OFF \
-        -DJAS_ENABLE_LIBHEIF=OFF
+        -DJAS_ENABLE_LIBHEIF=OFF \
+        -DJAS_STDC_VERSION=201710L
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
