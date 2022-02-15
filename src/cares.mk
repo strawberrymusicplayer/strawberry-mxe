7c54ae0f5e594713ba598f74af094955595f4cad
Jonas Kvinge <jonas@jkvinge.net>
2022-02-15 23:20:52 +0100

    Add cares

---
+++ src/cares.mk
@@ -0,0 +1,17 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := cares
+$(PKG)_WEBSITE  := https://c-ares.org/
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.18.1
+$(PKG)_CHECKSUM := 1a7d52a8a84a9fbffb1be9133c0f6e17217d91ea5a6fa61f6b4729cda78ebbcf
+$(PKG)_FILE     := c-ares-$($(PKG)_VERSION).tar.gz
+$(PKG)_SUBDIR   := c-ares-$($(PKG)_VERSION)
+$(PKG)_URL      := https://c-ares.org/download/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS)
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
