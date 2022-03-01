527536a952facdf422239ed3474f62122e9318bf
Jonas Kvinge <jonas@jkvinge.net>
2022-03-01 20:59:08 +0100

    Update harfbuzz

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz is a text shaping engine
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.4.0
-$(PKG)_CHECKSUM := 810bcd3d22fae3c2c18c3688455abc1cd0d7fb2fae25404890b0d77e6443bd0a
+$(PKG)_VERSION  := 4.0.0
+$(PKG)_CHECKSUM := 4880c25022100c31aef4bdea084be2fe58020f9756e94151b8d1cbf0be1ed54c
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
 $(PKG)_DEPS     := cc cairo freetype-bootstrap glib icu4c
 
