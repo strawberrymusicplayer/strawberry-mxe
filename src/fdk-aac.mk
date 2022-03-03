d9629ccece566f972ddfbf740b948e0bf56e32d3
Jonas Kvinge <jonas@jkvinge.net>
2022-03-03 23:47:01 +0100

    Add fdk-aac

---
+++ src/fdk-aac.mk
@@ -0,0 +1,26 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := fdk-aac
+$(PKG)_WEBSITE  := https://github.com/mstorsjo/fdk-aac
+$(PKG)_DESCR    := FDK-AAC
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.0.2
+$(PKG)_CHECKSUM := c9e8630cf9d433f3cead74906a1520d2223f89bcd3fa9254861017440b8eb22f
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/opencore-amr/$(PKG)/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://sourceforge.net/projects/opencore-amr/files/fdk-aac/' | \
+    $(SED) -n 's,.*fdk-aac-\([0-9.]*\)\.tar.*,\1,p' | \
+    $(SORT) -V | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS)
+    #CXXFLAGS='-Wno-narrowing'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
