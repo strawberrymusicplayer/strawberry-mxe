4019ec82492b244fd162ca365c6142cd8bcffd8a
Jonas Kvinge <jonas@jkvinge.net>
2022-03-08 21:52:12 +0100

    Switch back to libsoup 2

--- src/libsoup.mk
+++ src/libsoup.mk
@@ -4,8 +4,8 @@ PKG             := libsoup
 $(PKG)_WEBSITE  := https://github.com/GNOME/libsoup
 $(PKG)_DESCR    := HTTP client/server library for GNOME
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.4
-$(PKG)_CHECKSUM := 5bd38b5e091f707fd7fa3ed7c37aacca3f8e16c65787f1cc17dc38d1dcde567b
+$(PKG)_VERSION  := 2.74.2
+$(PKG)_CHECKSUM := f0a427656e5fe19e1df71c107e88dfa1b2e673c25c547b7823b6018b40d01159
 $(PKG)_SUBDIR   := libsoup-$($(PKG)_VERSION)
 $(PKG)_FILE     := libsoup-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/libsoup/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
@@ -14,12 +14,14 @@ $(PKG)_DEPS     := cc glib glib-networking sqlite brotli libpsl nghttp2 $(BUILD)
 define $(PKG)_UPDATE
     $(call MXE_GET_GH_TAGS,GNOME/libsoup) | \
     grep -v '\([0-9]\+\.\)\{2\}9[0-9]' | \
+    grep -v '2.99.' | \
+    grep -v '3.0.' | \
     $(SORT) -Vr | \
     head -1
 endef
 
 define $(PKG)_BUILD
-    cd '$(SOURCE_DIR)' && '$(TARGET)-meson' \
+    cd '$(SOURCE_DIR)' && LDFLAGS='$(LDFLAGS) -Wl,--allow-multiple-definition' '$(TARGET)-meson' \
         --buildtype='$(MESON_BUILD_TYPE)' \
         -Dtests=false \
         -Dvapi=disabled \
@@ -27,6 +29,9 @@ define $(PKG)_BUILD
         -Dintrospection=disabled \
         -Dtests=false \
         -Dsysprof=disabled \
+        -Dtls_check=false \
+        -Dgnome=false \
+        -Dgtk_doc=false \
         '$(BUILD_DIR)'
     cd '$(BUILD_DIR)' && ninja
     cd '$(BUILD_DIR)' && ninja install
