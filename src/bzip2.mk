1bd25fb0bd839457e75467aa5306810d12b4c056
Jonas Kvinge <jonas@jkvinge.net>
2022-03-13 18:26:04 +0100

    Update bzip2

--- src/bzip2.mk
+++ src/bzip2.mk
@@ -4,15 +4,15 @@ PKG             := bzip2
 $(PKG)_WEBSITE  := https://www.sourceware.org/bzip2/
 $(PKG)_DESCR    := freely available, patent free, high-quality data compressor
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.6
-$(PKG)_CHECKSUM := a2848f34fcd5d6cf47def00461fcb528a0484d8edef8208d6d2e2909dc61d9cd
+$(PKG)_VERSION  := 1.0.8
+$(PKG)_CHECKSUM := ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269
 $(PKG)_SUBDIR   := bzip2-$($(PKG)_VERSION)
 $(PKG)_FILE     := bzip2-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://ftp.osuosl.org/pub/clfs/conglomeration/bzip2/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL      := https://sourceware.org/pub/bzip2/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://ftp.osuosl.org/pub/clfs/conglomeration/bzip2/' | \
+    $(WGET) -q -O- 'https://sourceware.org/pub/bzip2/' | \
     grep 'bzip2-' | \
     $(SED) -n 's,.*bzip2-\([0-9][^>]*\)\.tar.*,\1,p' | \
     sort -V | \
