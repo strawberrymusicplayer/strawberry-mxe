97dfe7f2f019703b42a30133d5a72f67612cc51c
Jonas Kvinge <jonas@jkvinge.net>
2022-05-06 16:24:08 +0200

    Update binutils

--- src/binutils.mk
+++ src/binutils.mk
@@ -3,8 +3,8 @@
 PKG             := binutils
 $(PKG)_WEBSITE  := https://www.gnu.org/software/binutils/
 $(PKG)_DESCR    := GNU Binutils
-$(PKG)_VERSION  := 2.37
-$(PKG)_CHECKSUM := 820d9724f020a3e69cb337893a0b63c2db161dadcb0e06fc11dc29eb1e84a32c
+$(PKG)_VERSION  := 2.38
+$(PKG)_CHECKSUM := e316477a914f567eccc34d5d29785b8b0f5a10208d36bbacedcc39048ecfe024
 $(PKG)_SUBDIR   := binutils-$($(PKG)_VERSION)
 $(PKG)_FILE     := binutils-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/binutils/$($(PKG)_FILE)
@@ -14,7 +14,6 @@ $(PKG)_DEPS     :=
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://ftp.gnu.org/gnu/binutils/?C=M;O=D' | \
     $(SED) -n 's,.*<a href="binutils-\([0-9][^"]*\)\.tar.*,\1,p' | \
-    grep -v '2.38' | \
     $(SORT) -V | \
     tail -1
 endef
