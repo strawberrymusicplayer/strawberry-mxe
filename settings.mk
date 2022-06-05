3f44c42d3fb1f9dd440357bc63639d85ef61f0a7
Jonas Kvinge <jonas@jkvinge.net>
2022-06-06 00:47:41 +0200

    Remove i686-w64-mingw32.shared from settings.mk

--- settings.mk
+++ settings.mk
@@ -1,4 +1,4 @@
 JOBS := 8
-MXE_TARGETS := i686-w64-mingw32.shared x86_64-w64-mingw32.shared
+MXE_TARGETS := x86_64-w64-mingw32.shared
 MXE_BUILD_TYPE := Release
 SOURCEFORGE_MIRROR := downloads.sourceforge.net
