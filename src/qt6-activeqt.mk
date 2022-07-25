4545abf8bc3ef11ae071345aa4653ca376d4f458
Jonas Kvinge <jonas@jkvinge.net>
2022-07-25 04:14:22 +0200

    Update qt6-activeqt

--- src/qt6-activeqt.mk
+++ src/qt6-activeqt.mk
@@ -4,17 +4,17 @@ PKG             := qt6-activeqt
 $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt 6 Active Qt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.2.4
-$(PKG)_CHECKSUM := 486c1f2ccf0e545c68ebb5e139456f924b9e0948bdd16351701d624172e539f3
+$(PKG)_VERSION  := 6.3.1
+$(PKG)_CHECKSUM := 64b54e93bd3deb325c404ff7c7255b2769963b4096ebc22a97db788b257ebe2b
 $(PKG)_SUBDIR   := qtactiveqt-everywhere-src-$($(PKG)_VERSION)
 $(PKG)_FILE     := qtactiveqt-everywhere-src-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://download.qt.io/official_releases/qt/6.2/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
+$(PKG)_URL      := https://download.qt.io/official_releases/qt/6.3/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc qt6-qtbase
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- https://download.qt.io/official_releases/qt/6.2/ | \
-    $(SED) -n 's,.*href="\(6\.2\.[^/]*\)/".*,\1,p' | \
+    $(WGET) -q -O- https://download.qt.io/official_releases/qt/6.3/ | \
+    $(SED) -n 's,.*href="\(6\.3\.[^/]*\)/".*,\1,p' | \
     sort |
     tail -1
 endef
