4e143ebff144055e1d6df003e5262dbb912ffe68
Jonas Kvinge <jonas@jkvinge.net>
2022-10-02 21:09:28 +0200

    Add libjpeg-turbo

---
+++ src/libjpeg-turbo.mk
@@ -0,0 +1,36 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := libjpeg-turbo
+$(PKG)_WEBSITE  := https://libjpeg-turbo.org/
+$(PKG)_DESCR    := Free library for JPEG image compression
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.1.4
+$(PKG)_CHECKSUM := d3ed26a1131a13686dfca4935e520eb7c90ae76fbc45d98bb50a8dc86230342b
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc yasm
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://sourceforge.net/projects/libjpeg-turbo/files/' | \
+    $(SED) -n 's,.*/projects/.*/\([0-9][^"%]*\)/".*,\1,p' | \
+    $(SORT) -V | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DENABLE_SHARED=$(CMAKE_SHARED_BOOL) \
+        -DENABLE_STATIC=$(CMAKE_STATIC_BOOL) \
+        -DCMAKE_INSTALL_BINDIR='$(PREFIX)/$(TARGET)/bin/$(PKG)' \
+        -DCMAKE_INSTALL_INCLUDEDIR='$(PREFIX)/$(TARGET)/include/$(PKG)' \
+        -DCMAKE_INSTALL_LIBDIR='$(PREFIX)/$(TARGET)/lib/$(PKG)' \
+        -DCMAKE_ASM_NASM_COMPILER=$(TARGET)-yasm
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(TOP_DIR)/src/jpeg-test.c' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        `'$(TARGET)-pkg-config' '$(PREFIX)/$(TARGET)/lib/$(PKG)/pkgconfig/libjpeg.pc' --cflags --libs`
+endef
