78433acc02b5fafcf3bb64c344ec62f60c717392
Jonas Kvinge <jonas@jkvinge.net>
2022-10-14 21:58:31 +0200

    Update update.sh

--- tools/update.sh
+++ tools/update.sh
@@ -20,7 +20,132 @@
 repo="strawberrymusicplayer/strawberry-mxe"
 
 function timestamp() { date '+%Y-%m-%d %H:%M:%S'; }
+function status() { echo "[$(timestamp)] $*"; }
 function error() { echo "[$(timestamp)] ERROR: $*" >&2; }
+
+function update_repo() {
+
+  git fetch >/dev/null 2>&1 || exit 1
+  if [ $? -ne 0 ]; then
+    error "Could not fetch"
+    exit 1
+  fi
+
+  git checkout . >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    error "Could not checkout ."
+    exit 1
+  fi
+
+  if ! [ "$(git branch | head -1 | cut -d ' ' -f2)" = "master" ]; then
+    git checkout master >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      error "Could not checkout master branch."
+      exit 1
+    fi
+  fi
+
+  git pull origin master --rebase >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    error "Could not pull with rebase ."
+    exit 1
+  fi
+
+}
+
+function merge_prs() {
+
+  local prs
+  local pr
+  local review_decision
+  local status_check_rollup
+  local status_check_ok
+  local status_check_total
+
+  prs=$(gh pr list --json number | jq '.[].number')
+  if [ "${prs}" = "" ]; then
+    return
+  fi
+
+  for pr in ${prs}; do
+    if ! [ "$(gh pr view "${pr}" --json 'author' | jq -r '.author.login')" = "strawbsbot" ]; then
+      continue
+    fi
+    if ! [ "$(gh pr view "${pr}" --json 'isDraft' | jq '.isDraft')" = "false" ]; then
+      continue
+    fi
+    if ! [ "$(gh pr view "${pr}" --json 'mergeable' | jq -r '.mergeable')" = "MERGEABLE" ]; then
+      continue
+    fi
+    if ! [ "$(gh pr view "${pr}" --json 'mergeStateStatus' | jq -r '.mergeStateStatus')" = "CLEAN" ]; then
+      continue
+    fi
+    review_decision=$(gh pr view "${pr}" --json 'reviewDecision' | jq -r '.reviewDecision')
+    if [ ! "${review_decision}" = "" ] && [ ! "${review_decision}" = "APPROVED" ] ; then
+      continue
+    fi
+    status_check_rollup=$(gh pr view "${pr}" --json 'statusCheckRollup')
+    status_check_ok="1"
+    status_check_total=$(echo "${status_check_rollup}" | jq '.statusCheckRollup | length')
+    for ((i = 0; i < status_check_total; i++)); do
+      status_check=$(echo "${status_check_rollup}" | jq -r ".statusCheckRollup[${i}].status")
+      if ! [ "${status_check}" = "COMPLETED" ]; then
+        status_check_ok=0
+        break
+      fi
+    done
+    if ! [ "${status_check_ok}" = "1" ]; then
+      continue
+    fi
+    status "Merging pull request ${pr}."
+    gh pr merge -dr "${pr}"
+  done
+
+}
+
+function update_packages() {
+
+  make update
+  #if [ $? -ne 0 ]; then
+  #  echo "make update failed :("
+  #  exit 1
+  #fi
+
+  package_files=$(git ls-files src/*.mk --modified || exit 1)
+  if [ "${package_files}" = "" ]; then
+    return
+  fi
+
+  for package_file in ${package_files}; do
+
+    package_name=$(grep '^PKG\s*:= ' "${package_file}" | sed -e 's/^PKG\s*:= \(.*\)/\1/' || exit 1)
+    if [ "${package_name}" = "" ]; then
+      continue
+    fi
+
+    # Ignore qt6 packages except qt6-qtbase
+    if [ ! "$(echo "${package_name}" | grep '^qt6-')" = "" ] && [ "$(echo "${package_name}" | grep '^qt6-qtbase$')" = "" ]; then
+      continue
+    fi
+
+    # Ignore gstreamer plugins
+    if [ ! "$(echo "${package_name}" | grep -e '^gst-plugins-' -e '^gst-libav$')" = "" ]; then
+      continue
+    fi
+
+    update_package "${package_file}"
+
+    if ! [ "$(git branch | head -1 | cut -d ' ' -f2)" = "master" ]; then
+      git checkout master >/dev/null 2>&1
+      if [ $? -ne 0 ]; then
+        error "Could not checkout master branch."
+        return
+      fi
+    fi
+
+  done
+
+}
 
 function update_package() {
 
@@ -33,22 +158,22 @@ function update_package() {
 
   package_file="${1}"
 
-  package_name=$(grep '^PKG\s*:= ' ${package_file} | sed -e 's/^PKG\s*:= \(.*\)/\1/' || exit 1)
+  package_name=$(grep '^PKG\s*:= ' "${package_file}" | sed -e 's/^PKG\s*:= \(.*\)/\1/' || exit 1)
   if [ "${package_name}" = "" ]; then
-    echo "Could not get package name for ${package_file}"
+    error "Could not get package name for ${package_file}"
     return
   fi
 
-  package_version=$(grep '$(PKG)_VERSION\s\+:=\s\+' ${package_file} | sed -e 's/^\$(PKG)_VERSION\s\+:=\s\+\(.*\)/\1/' || exit 1)
+  package_version=$(grep '$(PKG)_VERSION\s\+:=\s\+' "${package_file}" | sed -e 's/^\$(PKG)_VERSION\s\+:=\s\+\(.*\)/\1/' || exit 1)
   if [ "${package_version}" = "" ]; then
-    echo "Could not get package version for ${package_file}"
+    error "Could not get package version for ${package_file}"
     return
   fi
 
   # Make sure we have a checksum, otherwise the update failed.
-  grep '^\$(PKG)_CHECKSUM\s\+:=\s\+$' ${package_file} >/dev/null 2>&1
+  grep '^\$(PKG)_CHECKSUM\s\+:=\s\+$' "${package_file}" >/dev/null 2>&1
   if [ $? -eq 0 ]; then
-    echo "Skipping ${package_name} (${package_file}), invalid checksum."
+    error "Skipping ${package_name} (${package_file}), invalid checksum."
     git checkout "${package_file}"
     return
   fi
@@ -68,12 +193,12 @@ function update_package() {
 
   git branch | grep "${package_branch}" >/dev/null 2>&1
   if [ $? -eq 0 ]; then
-    echo "Skipping ${package_shortname} (${package_files}), branch for version ${package_version} already exists."
+    status "Skipping ${package_shortname} (${package_files}), branch for version ${package_version} already exists."
     git checkout ${package_files}
     return
   fi
 
-  echo "${package_shortname}: ${package_version}"
+  status "${package_shortname}: ${package_version}"
   git checkout -b "${package_branch}" || exit 1
   git add ${package_files} || exit 1
   git commit -m "Update ${package_shortname}" ${package_files} || exit 1
@@ -82,9 +207,19 @@ function update_package() {
   if ! [ "$(git branch | head -1 | cut -d ' ' -f2)" = "master" ]; then
     git checkout master >/dev/null 2>&1
     if [ $? -ne 0 ]; then
-      echo "Could not checkout master branch."
+      error "Could not checkout master branch."
       exit 1
     fi
+  fi
+
+}
+
+function cleanup() {
+
+  rm -rf log/*
+  # Remove downloaded packages every month.
+  if [ "$(date +%d)" = "01" ]; then
+    rm -f pkg/*
   fi
 
 }
@@ -111,35 +246,35 @@ fi
 dir="$(dirname "$0")"
 
 if [ "${dir}" = "" ]; then
-  echo "Could not get current directory."
+  error "Could not get current directory."
   exit 1
 fi
 
 if ! [ -d "${dir}" ]; then
-  echo "Missing ${dir}"
+  error "Missing ${dir}"
   exit 1
 fi
 
 if ! [ -d "${dir}/../.git" ]; then
-  echo "Missing ${dir}/../.git"
+  error "Missing ${dir}/../.git"
   exit 1
 fi
 
 if ! [ -f "${dir}/../Makefile" ]; then
-  echo "Missing ${dir}/../Makefile"
+  error "Missing ${dir}/../Makefile"
   exit 1
 fi
 
 repodir="$(dirname "${dir}")"
 
 if ! [ -d "${repodir}" ]; then
-  echo "Missing ${repodir}."
+  error "Missing ${repodir}."
   exit 1
 fi
 
 cd "${repodir}"
 if [ $? -ne 0 ]; then
-  echo "Could not change directory to ${repodir}."
+  error "Could not change directory to ${repodir}."
   exit 1
 fi
 
@@ -155,82 +290,12 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-git fetch >/dev/null 2>&1 || exit 1
-if [ $? -ne 0 ]; then
-  echo "Could not fetch"
-  exit 1
-fi
+update_repo
+merge_prs
 
-git checkout . >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "Could not checkout ."
-  exit 1
-fi
+update_repo
+update_packages
 
-if ! [ "$(git branch | head -1 | cut -d ' ' -f2)" = "master" ]; then
-  git checkout master >/dev/null 2>&1
-  if [ $? -ne 0 ]; then
-    echo "Could not checkout master branch."
-    exit 1
-  fi
-fi
+update_repo
 
-git pull origin master --rebase >/dev/null
-if [ $? -ne 0 ]; then
-  echo "Could not pull with rebase ."
-  exit 1
-fi
-
-make update
-#if [ $? -ne 0 ]; then
-#  echo "make update failed :("
-#  exit 1
-#fi
-
-package_files=$(git ls-files src/*.mk --modified || exit 1)
-if [ "${package_files}" = "" ]; then
-  exit 0
-fi
-
-for package_file in ${package_files}; do
-
-  package_name=$(grep '^PKG\s*:= ' ${package_file} | sed -e 's/^PKG\s*:= \(.*\)/\1/' || exit 1)
-  if [ "${package_name}" = "" ]; then
-    continue
-  fi
-
-  # Ignore qt6 packages except qt6-qtbase
-  if [ ! "$(echo "${package_name}" | grep '^qt6-')" = "" ] && [ "$(echo "${package_name}" | grep '^qt6-qtbase$')" = "" ]; then
-    continue
-  fi
-
-  # Ignore gstreamer plugins
-  if [ ! "$(echo "${package_name}" | grep -e '^gst-plugins-' -e '^gst-libav$')" = "" ]; then
-    continue
-  fi
-
-  update_package "${package_file}"
-
-  if ! [ "$(git branch | head -1 | cut -d ' ' -f2)" = "master" ]; then
-    git checkout master >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-      echo "Could not checkout master branch."
-      exit 1
-    fi
-  fi
-
-done
-
-git checkout . >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "Could not checkout ."
-  exit 1
-fi
-
-git pull origin master --rebase >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "Could not pull with rebase ."
-  exit 1
-fi
-
-rm -f pkg/*
+cleanup
