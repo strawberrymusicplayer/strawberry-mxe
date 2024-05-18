567cce52bfb101b52dd72e59a010144ea44aa0c9
Jonas Kvinge <jonas@jkvinge.net>
2024-05-18 21:14:58 +0200

    harfbuzz: Fix download

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -5,8 +5,11 @@ $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz is a text shaping engine
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 8.5.0
-$(PKG)_CHECKSUM := 57ec8c49152fe417939a8e74750dd1e05ad7b8e9084eb42d24c9919369e8e7ff
+$(PKG)_CHECKSUM := 77e4f7f98f3d86bf8788b53e6832fb96279956e1c3961988ea3d4b7ca41ddc27
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
+$(PKG)_SUBDIR   := harfbuzz-$($(PKG)_VERSION)
+$(PKG)_FILE     := harfbuzz-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://github.com/harfbuzz/harfbuzz/releases/download/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc freetype-bootstrap glib icu4c cairo
 
 define $(PKG)_BUILD
