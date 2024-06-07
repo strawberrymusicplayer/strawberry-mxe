d1e6ba088e68a91c98b29cf285c16a358bbd29ad
Jonas Kvinge <jonas@jkvinge.net>
2024-06-07 20:35:10 +0200

    cares: Change download URL

--- src/cares.mk
+++ src/cares.mk
@@ -8,7 +8,7 @@ $(PKG)_VERSION  := 1.29.0
 $(PKG)_CHECKSUM := 0b89fa425b825c4c7bc708494f374ae69340e4d1fdc64523bdbb2750bfc02ea7
 $(PKG)_FILE     := c-ares-$($(PKG)_VERSION).tar.gz
 $(PKG)_SUBDIR   := c-ares-$($(PKG)_VERSION)
-$(PKG)_URL      := https://c-ares.org/download/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/c-ares/c-ares/releases/download/cares-$(subst .,_,$($(PKG)_VERSION))/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
