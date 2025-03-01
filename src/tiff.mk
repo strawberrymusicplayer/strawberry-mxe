0ab432409fa668f4311f8b1564f9900ee138e311
Jonas Kvinge <jonas@jkvinge.net>
2025-03-01 11:45:08 +0100

    Fix tiff URLs

--- src/tiff.mk
+++ src/tiff.mk
@@ -1,7 +1,7 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := tiff
-$(PKG)_WEBSITE  := http://simplesystems.org/libtiff/
+$(PKG)_WEBSITE  := http://www.simplesystems.org/libtiff/
 $(PKG)_DESCR    := TIFF Library and Utilities
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.7.0
@@ -12,7 +12,7 @@ $(PKG)_URL      := https://download.osgeo.org/libtiff/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc jpeg libwebp xz zlib
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://simplesystems.org/libtiff/' | \
+    $(WGET) -q -O- 'http://www.simplesystems.org/libtiff/' | \
     $(SED) -n 's,.*>v\([0-9][^<]*\)<.*,\1,p' | \
     head -1
 endef
