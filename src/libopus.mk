6dd5e420b77c510afa6c37f192b81fa6766ed5d0
Jonas Kvinge <jonas@jkvinge.net>
2025-03-17 20:15:20 +0100

    libopus: Use GitHub for update

--- src/libopus.mk
+++ src/libopus.mk
@@ -9,17 +9,9 @@ $(PKG)_CHECKSUM := 65c1d2f78b9f2fb20082c38cbe47c951ad5839345876e46941612ee87f9a7
 $(PKG)_SUBDIR   := opus-$($(PKG)_VERSION)
 $(PKG)_FILE     := opus-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://downloads.xiph.org/releases/opus/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://github.com/xiph/opus/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_GH_CONF  := xiph/opus/releases/latest, v
 $(PKG)_DEPS     := cc
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://downloads.xiph.org/releases/opus/?C=M;O=D' | \
-    $(SED) -n 's,.*opus-\([0-9][^>]*\)\.tar.*,\1,p' | \
-    grep -v 'alpha' | \
-    grep -v 'beta' | \
-    grep -v 'rc' | \
-    $(SORT) -V | \
-    tail -1
-endef
 
 define $(PKG)_BUILD
     cd '$(1)' && $(SHELL) ./configure $(MXE_CONFIGURE_OPTS) --disable-doc
