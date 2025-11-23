3b5bac412822489c3ccea4302deead64383de970
Jonas Kvinge <jonas@jkvinge.net>
2025-11-23 18:53:18 +0100

    Update libgme

--- src/libgme.mk
+++ src/libgme.mk
@@ -1,22 +1,16 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libgme
-$(PKG)_WEBSITE  := https://bitbucket.org/mpyne/game-music-emu/
+$(PKG)_WEBSITE  := https://github.com/libgme/game-music-emu
 $(PKG)_DESCR    := Collection of video game music file emulators
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.6.3
-$(PKG)_CHECKSUM := aba34e53ef0ec6a34b58b84e28bf8cfbccee6585cebca25333604c35db3e051d
-$(PKG)_FILE     := game-music-emu-$($(PKG)_VERSION).tar.xz
+$(PKG)_VERSION  := 0.6.4
+$(PKG)_CHECKSUM := f2360feb5a32ace226c583df4faf6eff74145c81264aaea11e17a1af2f6f101a
+$(PKG)_GH_CONF  := libgme/game-music-emu/releases/latest
+$(PKG)_FILE     := game-music-emu-$($(PKG)_VERSION)-src.tar.gz
 $(PKG)_SUBDIR   := game-music-emu-$($(PKG)_VERSION)
-$(PKG)_URL      := https://bitbucket.org/mpyne/game-music-emu/downloads/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/libgme/game-music-emu/releases/download/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://bitbucket.org/mpyne/game-music-emu/downloads/' | \
-    $(SED) -n 's,.*game-music-emu-\([^>]*\)\.tar.*,\1,p' | \
-    $(SORT) -V | \
-    tail -1
-endef
 
 define $(PKG)_BUILD
     '$(TARGET)-cmake' -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)' \
