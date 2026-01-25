24907892a009e9ad628d5ff0ea2111792c92aeb1
Jonas Kvinge <jonas@jkvinge.net>
2026-01-25 01:12:40 +0100

    tiff: Build with cmake

--- src/tiff.mk
+++ src/tiff.mk
@@ -9,7 +9,7 @@ $(PKG)_CHECKSUM := f698d94f3103da8ca7438d84e0344e453fe0ba3b7486e04c5bf7a9a3fabe9
 $(PKG)_SUBDIR   := tiff-$($(PKG)_VERSION)
 $(PKG)_FILE     := tiff-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://download.osgeo.org/libtiff/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc jpeg libwebp xz zlib
+$(PKG)_DEPS     := cc jpeg libwebp xz zlib getopt-win
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://www.simplesystems.org/libtiff/' | \
@@ -18,7 +18,14 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS) --disable-rpath --without-x
-    $(MAKE) -C '$(1)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT)
-    $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_CRUFT)
+    '$(TARGET)-cmake' -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)' \
+        -DCMAKE_BUILD_TYPE='$(MXE_BUILD_TYPE)' \
+        -DBUILD_SHARED_LIBS=$(CMAKE_SHARED_BOOL) \
+        -DBUILD_STATIC_LIBS=$(CMAKE_STATIC_BOOL) \
+        -Dtiff-static=$(CMAKE_STATIC_BOOL) \
+        -Djpeg=ON \
+        -Dtiff-docs=OFF \
+        -Dtiff-tests=OFF
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
