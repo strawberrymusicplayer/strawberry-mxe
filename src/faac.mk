a6075efc34340918f39711924bb75cce20932cee
Jonas Kvinge <jonas@jkvinge.net>
2026-04-16 20:47:30 +0200

    faac: Add meson-conf to deps

--- src/faac.mk
+++ src/faac.mk
@@ -7,7 +7,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.50
 $(PKG)_CHECKSUM := e6876cba00cbd786a7f984d9aaada4d5bcb08d2582100366c70f6164d5c89214
 $(PKG)_GH_CONF  := knik0/faac/releases/latest, faac-
-$(PKG)_DEPS     := cc
+$(PKG)_DEPS     := cc meson-conf
 
 define $(PKG)_BUILD
     cd '$(SOURCE_DIR)' && '$(TARGET)-meson' --buildtype='$(MESON_BUILD_TYPE)' '$(BUILD_DIR)'
